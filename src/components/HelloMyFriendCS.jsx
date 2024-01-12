f632dfc09b25dca89f210c43534ec096cceda0ae
Caro Valverde <carolina.valverde255@gmail.com>
2024-01-12 15:47:25 -0300

    agrego codigo funciona

--- src/components/HelloMyFriendCS.jsx
+++ src/components/HelloMyFriendCS.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 const HelloWorld = ({ name }) => {
+  const [msj, setMsj] = useState("(from changed state)");
+
+  const handleClick = () => {
+    setMsj("(changed state)");
+  };
+
   return (
     <div className="container-fluid text-bg-dark text-center p-2">
-      <h1>Hello {name}!</h1>
+      <h1>
+        Hello {name}! {msj}
+      </h1>
+      <button className="btn btn-primary m-2" onClick={handleClick}>
+        Click me
+      </button>
     </div>
   );
 };

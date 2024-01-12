f4bb7b5b1b99d3e5cbb0769319921c3617f7cd8b
Caro Valverde <carolina.valverde255@gmail.com>
2024-01-12 15:28:51 -0300

    agrego codigo del ej anterior

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import HelloWorld from "./components/HelloMyFriendCS";
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld name="my friend" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HelloMyFriendCS.jsx
@@ -0,0 +1,9 @@
+const HelloWorld = ({ name }) => {
+  return (
+    <div className="container-fluid text-bg-dark text-center p-2">
+      <h1>Hello {name}!</h1>
+    </div>
+  );
+};
+
+export default HelloWorld;
